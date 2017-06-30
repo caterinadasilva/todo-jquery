2bdbd2cc26effb1964e054e51687f6882c13b5f6
Syrimande <caterinadasilva@gmail.com>
2017-06-30 19:29:53 -0400

    Ajuster de diseño

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
+var contadorItems = 1;
+
 $(document).ready(function () {
 	$('#list-items').html(localStorage.getItem('listItems'));
 	$('.add-items').submit(function(event) {
 		event.preventDefault();
 		var item = $('#todo-list-item').val();
 		if(item) {
-			$('#list-items').append('<li class="collection-item"><input type="checkbox" id="' + item + '" /><label for="' + item + '">' + item + '</label><a href="#!" class="secondary-content remove"><i class="material-icons">delete</i></a></li>');
+			console.log(contadorItems);
+			$('#list-items').append('<li class="collection-item"><input type="checkbox" id="item' + contadorItems + '" /><label for="item' + contadorItems + '">' + item + '</label><a href="#!" class="secondary-content remove"><i class="material-icons">delete</i></a></li>');
 			localStorage.setItem('listItems', $('#list-items').html());
 			$('#todo-list-item').val("");
 		}
 	});
-	$(document).on('change', '.checkbox', function() {
-		if($(this).attr('checked')) {
-			//$(this).attr('checked', 'checked');
+	$(document).on('change', ':checkbox', function() {
+		if(this.checked) {
 			$(this).parent().appendTo('complete-items');
 			localStorage.setItem('listItems', $('#complete-items').html());
 		} else {
-			$(this).removeAttr('checked');
+			console.log(this.id + " is unchecked.")
 		}
 	});
 	$(document).on('click', '.remove', function() {
-		$(this).parent().parent().remove();
+		$(this).parent().remove();
 		localStorage.setItem('listItems', $('#list-items').html());
 	});
 
